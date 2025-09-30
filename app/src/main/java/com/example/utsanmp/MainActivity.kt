ce29baac5975dcf1eb5a84492e0ecd3e472efb6a
soykevv <sumayouwkevin@gmail.com>
2025-09-30 14:06:51 +0700

    bottom nav bar

--- app/src/main/java/com/example/utsanmp/MainActivity.kt
+++ app/src/main/java/com/example/utsanmp/MainActivity.kt
@@ -5,15 +5,23 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.utsanmp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        navController = (supportFragmentManager.findFragmentById(R.id.hostFragment) as NavHostFragment).navController
+        binding.bottomNav.setupWithNavController(navController)
     }
 }
