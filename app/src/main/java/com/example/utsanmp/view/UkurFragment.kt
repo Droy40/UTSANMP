8864286cf4cc4a5cbe98d788eed6f5d92218f633
soykevv <sumayouwkevin@gmail.com>
2025-09-30 13:51:38 +0700

    fragment data ukur

--- app/src/main/java/com/example/utsanmp/view/UkurFragment.kt
+++ app/src/main/java/com/example/utsanmp/view/UkurFragment.kt
@@ -6,26 +6,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
 import com.example.utsanmp.R
+import com.example.utsanmp.databinding.FragmentUkurBinding
+import com.example.utsanmp.model.DataUkur
+import com.example.utsanmp.viewmodel.UkurViewModel
 
 class UkurFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = UkurFragment()
-    }
-
-    private val viewModel: UkurViewModel by viewModels()
+    private lateinit var binding: FragmentUkurBinding
+    private lateinit var viewmodel: UkurViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // TODO: Use the ViewModel
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_ukur, container, false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentUkurBinding.inflate(inflater, container, false
+        )
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewmodel = ViewModelProvider(this).get(UkurViewModel::class.java)
+
+        binding.btnTambahData.setOnClickListener {
+            val beratBadan = binding.txtBeratBadan.text.toString().toIntOrNull() ?: 0
+            val tinggiBadan = binding.txtTinggiBadan.text.toString().toIntOrNull() ?: 0
+            val usia = binding.txtUsia.text.toString().toIntOrNull() ?: 0
+
+            viewmodel.saving(DataUkur(beratBadan, tinggiBadan, usia))
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/utsanmp/viewmodel/UkurViewModel.kt
+++ app/src/main/java/com/example/utsanmp/viewmodel/UkurViewModel.kt
@@ -1,7 +1,38 @@
 package com.example.utsanmp.viewmodel
 
+import android.app.Application
+import android.util.Log
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.example.utsanmp.model.DataUkur
+import com.example.utsanmp.util.FileHelper
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
-class UkurViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class UkurViewModel(app: Application): AndroidViewModel(app) {
+        fun saving(dataUkur: DataUkur){
+            val filehelper = FileHelper(getApplication())
+            val sType = object : TypeToken<List<DataUkur>>() { }.type
+            val fromFile = filehelper.readFromFile()
+            Log.d("print_file", fromFile)
+
+            //bikin arrayList kosong
+            var dataList = mutableListOf<DataUkur>()
+
+            try {
+                //convert data json to ArrayList
+                dataList = Gson().fromJson<List<DataUkur>>(fromFile, sType).toMutableList()
+            }catch (e:Exception){
+
+            }
+
+            //add new parameter or new data to array lis
+            dataList.add(dataUkur)
+
+            //convert data to json
+            val jsonstring = Gson().toJson(dataList)
+
+            filehelper.writeToFile(jsonstring)
+            Log.d("print_file", jsonstring)
+        }
 }
