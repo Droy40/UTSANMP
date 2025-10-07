dfb83830aa2da6cf0a1aeb27ab109e16a13ad0fb
salmann17 <salmanlfaa11@gmail.com>
2025-10-07 11:55:54 +0700

    create DataListAdapter

---
+++ app/src/main/java/com/example/utsanmp/view/DataListAdapter.kt
@@ -0,0 +1,4 @@
+package com.example.utsanmp.view
+
+class DataListAdapter {
+}
--- app/src/main/java/com/example/utsanmp/viewmodel/DataViewModel.kt
+++ app/src/main/java/com/example/utsanmp/viewmodel/DataViewModel.kt
@@ -3,5 +3,5 @@ package com.example.utsanmp.viewmodel
 import androidx.lifecycle.ViewModel
 
 class DataViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
 }
